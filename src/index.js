459f6c9559f8fe7a24670e4bed562ff0760148f4
Ionut Pistol <ionut.pistol@gmail.com>
2017-11-20 08:59:06 +0200

    Implemented store, reducer, actions, dispatched

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './js/Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const initialState = { 
+    count: 0
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
 
 const App = () => (
-    <div>
+    <Provider store={ store }>
         <Counter />
-    </div>
+    </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/js/Counter.js
+++ src/js/Counter.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
 
 class Counter extends Component {
-    state = { count: 0 };
-
     increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
+        this.props.dispatch({ type: 'INCREMENT'});
     }
 
     decrement = () => {
-        this.setState({
-            count: this.state.count - 1
-        });
+        this.props.dispatch({ type: 'DECREMENT'});
     }
 
     render() {
@@ -21,7 +16,7 @@ class Counter extends Component {
                 <h2>Counter</h2>
                 <div>
                     <button onClick={ this.decrement }>-</button>
-                    <span>{ this.state.count }</span>
+                    <span>{ this.props.count }</span>
                     <button onClick={ this.increment }>+</button>
                 </div>
             </div>
@@ -29,4 +24,10 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    };
+}
+
+export default connect(mapStateToProps)(Counter);
